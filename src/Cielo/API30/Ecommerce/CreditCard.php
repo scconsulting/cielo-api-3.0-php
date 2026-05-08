22662b05a200a6dbeb40cb8ab143a04978a485e2
pradeep3093 <pradeep3093@gmail.com>
2026-05-08 18:29:37 +0530

    credit card brand check function added

--- src/Cielo/API30/Ecommerce/CreditCard.php
+++ src/Cielo/API30/Ecommerce/CreditCard.php
@@ -82,6 +82,65 @@ class CreditCard implements \JsonSerializable, CieloSerializable
     private $links;
 
     /**
+     * Detect card brand from card number using BIN patterns.
+     * Returns one of the brand constants or null if unrecognised.
+     *
+     * @param string $cardNumber digits only (spaces/dots stripped by caller)
+     * @return string|null
+     */
+    public static function detectBrand($cardNumber)
+    {
+        $number = preg_replace('/\D/', '', $cardNumber);
+
+        // Elo (Brazilian) — checked before Visa/Master due to BIN overlap
+        if (preg_match('/^(4011(67|78|79)|43(1274|8935)|45(1416|7393|763(1|2))|504175|627780|636297|636368|6504(0[5-9]|1[0-9]|2[0-9]|3[0-9])|6505(4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9])|6507(0[0-9]|1[0-8])|6509(0[1-9]|1[0-9]|20)|6516(5[2-9]|6[0-9]|7[0-9])|6550(0[0-9]|1[0-9]|2[1-9]|3[0-9]|4[0-9]|5[0-8]))/', $number)) {
+            return self::ELO;
+        }
+
+        // Hipercard (Brazilian)
+        if (preg_match('/^(606282|637095|637568|637599|637612|637609)/', $number)) {
+            return self::HIPERCARD;
+        }
+
+        // Aura (Brazilian)
+        if (preg_match('/^50[0-9]/', $number)) {
+            return self::AURA;
+        }
+
+        // Amex
+        if (preg_match('/^3[47]/', $number)) {
+            return self::AMEX;
+        }
+
+        // Diners
+        if (preg_match('/^3(0[0-5]|[68])/', $number)) {
+            return self::DINERS;
+        }
+
+        // Discover
+        if (preg_match('/^6(011|4[4-9]|5)/', $number)) {
+            return self::DISCOVER;
+        }
+
+        // JCB
+        if (preg_match('/^35(2[89]|[3-8][0-9])/', $number)) {
+            return self::JCB;
+        }
+
+        // Mastercard (classic 51-55 and new 2221-2720 range)
+        if (preg_match('/^(5[1-5]|2(2[2-9][1-9]|[3-6][0-9]{3}|7[01][0-9]|720))/', $number)) {
+            return self::MASTERCARD;
+        }
+
+        // Visa
+        if (preg_match('/^4/', $number)) {
+            return self::VISA;
+        }
+
+        return null;
+    }
+
+    /**
      * @param string $json
      *
      * @return CreditCard
